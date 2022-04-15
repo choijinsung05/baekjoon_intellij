c4885d37528516cab9b36ae2edc684c67001d08f
자기전양치질 <68370338+choijinsung05@users.noreply.github.com>
2022-04-16 01:50:40 +0900

    2022/04/16 백준 1181 단어정렬

--- src/q1181.java
+++ src/q1181.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class q1181 {
     public static void main(String args[]) throws IOException {
@@ -14,12 +15,28 @@ public class q1181 {
         for(int i = 0;i<n;i++){
             arr[i] = br.readLine();
         }
-
-
-
-        for(int i = arr.length-1;i>=0;i--){
-            System.out.print(arr[i]);
-        }
         br.close();
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if(s1.length() == s2.length()){
+                    return s1.compareTo(s2);
+                }
+                else{
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]).append('\n');
+
+        for(int i = 1; i < n;i++){
+            if(!arr[i].equals(arr[i-1])){
+                sb.append(arr[i]).append('\n');
+            }
+        }
+        System.out.println(sb);
     }
 }
