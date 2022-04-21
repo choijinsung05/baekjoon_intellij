52e297c9eb1f8b7e81256926318022d6bd1dcf32
자기전양치질 <68370338+choijinsung05@users.noreply.github.com>
2022-04-22 01:46:21 +0900

    ~2022/04/21
    2022/04/22
    10828 스택 구현 o - top 구현에서 빈 배열 생각 못해서 2번틀림
    
    StringBuilder 사용법 조금 더 숙지
    배열 구현 방법 더 알아보기
    사용법이 익숙하지가 않다 더 공부가 필요할꺼 같다.

---
+++ src/q11651.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class q11651 {
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int [][] arr = new int[n][2];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            if(i != n-1)st = new StringTokenizer(br.readLine());
+        }
+
+        Arrays.sort(arr, (a1, a2)->{
+            if(a1[1] == a2[1]){
+                return a1[0] - a2[0];
+            }else{
+                return a1[1] - a2[1];
+            }
+        });
+        for(int i = 0; i<n;i++){
+            System.out.println(arr[i][0]+" "+arr[i][1]);
+        }
+    }
+}
---
+++ src/q1436.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q1436 {
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int k = 1;
+        int answer = 666;
+        while(k<n){
+            answer++;
+            if(String.valueOf(answer).contains("666")){//이부분 공부하기!!!
+                k++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
