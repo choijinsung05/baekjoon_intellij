34692c7db813aa8e63dd1fae51eec5d9d7c27841
자기전양치질 <68370338+choijinsung05@users.noreply.github.com>
2022-06-17 08:40:03 +0900

    첫번째 시도 : 2579 계단문제와 동일하다고 생각하고 풀었다 하지만 2개의 포도주를 무시해야되는 경우가 생긴다? ex) 10 13 1 2 20 16 해결방법을 찾아 보자
    마지막 음료 k 번째 경우의 수를 확인해 보자
    k번째 음료수를 먹지 않을때  - a
    k번째 음료수를 먹을때 - b
    
    case a
    k를 제외한 음료를 어떻게 먹을지 결정하면 됨
    표현식 f(k) = f(k-1) ----k 안먹을때
    k-1 k
     ?  x
    case b
    k 먹는다. + arr[k]
    k-3 k-2 k-1 k
     ?   x   o  o
         ?   x  o

--- src/q2156.java
+++ src/q2156.java
@@ -3,33 +3,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class q2156 {
-    public static void main(String [] args)throws IOException{
+
+    static int [] arr;
+    static Integer [] dp;
+
+    public static void main(String [] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        int [] arr = new int[n+1];
-        int [] next1 = new int[n+1];//이전에 골랐던 포도주 바로 다음것을 선택한경우
-        int [] next2 = new int[n+1];//이전의 골랐던 포도주 이후 2번째 것을 선택한경우
+        arr = new int[n+1];
+        dp = new Integer[n+1];
 
-        int max_n = 0;
 
-        arr[1] = Integer.parseInt(br.readLine());
-        next1[1] = arr[1];
-        max_n = arr[1];
-        if(n>=2){
-            arr[2] = Integer.parseInt(br.readLine());
-            next1[2] = arr[1]+arr[2];
-            next2[2] = arr[2];
-            max_n = next1[2];
-        }
-
-        for(int i = 3;i<=n;i++){
+        for(int i = 1;i<=n;i++){
             arr[i] = Integer.parseInt(br.readLine());
-            next1[i] = next2[i-1]+arr[i];
-            next2[i] = next1[i-2]+arr[i];
-            max_n = Math.max(max_n,Math.max(next1[i],next2[i]));
         }
-        System.out.println(max_n);
+        dp[0] = 0;
+        dp[1] = arr[1];
+        if(n>1){
+            dp[2] = arr[1]+arr[2];
+        }
+        System.out.println(amount(n));
+    }
+    static int amount(int num){
+        if(dp[num] == null){
+            dp[num] = Math.max(amount(num-1),arr[num] + Math.max(arr[num-1]+ amount(num-3),amount(num-2)));
+        }
+        return dp[num];
     }
 }
