6d5ce43a58f84d86e2290c4e35fb68f4e6a7be74
자기전양치질 <68370338+choijinsung05@users.noreply.github.com>
2022-07-20 01:50:20 +0900

    연결리스트 관련 offer에 대해서 찾아 볼것.
    풀이관련
    1.연결리스트에 처음 순서와 중요도묶음인 배열을 하나씩 받는다.
    2.연결리스트에 첫번쨰 요소(now)를 가져온다.
    3.나머지 요소들중 중요도가 더 큰 파일(k)이 있는지 검사한다.
    3-1 있을경우
     now를 연결리스트에 다시 넣는다.
     now 와 k사이 요소들도 뒤로 넘긴다.
    3-2 없을경우
     출력횟수(count) 증가 후 뽑아야되는 문서 m과 비교
     맞으면 종료후 출력
     틀리면 다시 2번부터 반복

--- src/q1966.java
+++ src/q1966.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class q1966 {
@@ -21,17 +20,41 @@ public class q1966 {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
-            Queue<Integer>  que = new LinkedList<>();
+            LinkedList<int[]>  que = new LinkedList<>();
 
             st = new StringTokenizer(br.readLine()," ");
 
             for(int i = 0;i<n;i++) {
-                int k = Integer.parseInt(st.nextToken());
-                que.add(k);
+                que.offer(new int[]{i, Integer.parseInt(st.nextToken())});
             }
+            int count = 0;
+            while (!que.isEmpty()) {
+                int[] now = que.poll();
 
-            sb.append("\n");
+                Boolean is_print = true;//출력해야되는지 확인하는 변수
+
+                for (int i = 0; i < que.size(); i++) {
+                    if (now[1] < que.get(i)[1]) {
+
+                        que.offer(now);
+
+                        for (int j = 0; j < i; j++) {
+                            que.offer(que.poll());
+                        }
+                        is_print = false;
+                        break;
+                    }
+                }
+                if(!is_print)continue;
+
+                count++;
+                if(now[0] == m) {
+                    break;
+                }
+            }
+            sb.append(count).append("\n");
             tc--;
         }
+        System.out.println(sb);
     }
 }
