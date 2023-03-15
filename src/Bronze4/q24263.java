ceebb20693dcdf4c79c6bddb6d6be4f95ad235ad
자기전양치질 <68370338+choijinsung05@users.noreply.github.com>
2023-03-16 02:40:54 +0900

    파일 날아가서 다시 git공부겸 첫 코딩문제 제출

---
+++ src/Bronze4/q24263.java
@@ -0,0 +1,14 @@
+package Bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q24263 {
+    public static void main(String [] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine()+"\n1");
+
+    }
+}
