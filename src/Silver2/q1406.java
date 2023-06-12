9b3668b74d1a473f6cefe509c6ed43bb56c588eb
자기전양치질 <68370338+choijinsung05@users.noreply.github.com>
2023-06-12 23:13:00 +0900

    5/19 에디터(1406) #자료구조 #스택 커서옮기면서 텍스트 출력 자료구조 스택을 활용하여 커서의 왼쪽과 오른쪽을 각각 구분하여서 입력후 출력

---
+++ src/Silver2/q1406.java
@@ -0,0 +1,60 @@
+package Silver2;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class q1406 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = null;
+
+        String str = br.readLine();
+
+        Stack<Character> left = new Stack<Character>();
+        Stack<Character> right = new Stack<Character>();
+
+        for (int i = 0; i < str.length(); i++) {
+            left.push(str.charAt(i));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        while(M-->0){
+            st = new StringTokenizer(br.readLine()," ");
+
+            char c = st.nextToken().charAt(0);
+
+           switch (c){
+               case 'L':
+                   if(!left.isEmpty())
+                       right.push(left.pop());
+                   break;
+               case 'D':
+                   if(!right.isEmpty())
+                       left.push(right.pop());
+                   break;
+               case 'B':
+                   if(!left.isEmpty())
+                       left.pop();
+                   break;
+               case 'P':
+                   char t = st.nextToken().charAt(0);
+                   left.push(t);
+                   break;
+               default:
+                   break;
+           }
+
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!left.isEmpty()) {
+            right.push(left.pop());
+        }
+        while(!right.isEmpty()){
+            sb.append(right.pop());
+        }
+        System.out.println(sb);
+    }
+}
